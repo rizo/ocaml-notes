7c34e62d3f475d358b52c80e2f4e13cd722c0de8
Karel Moravec <karel@pusher.com>
2017-09-19 17:57:20 +0100

    Progres

--- src/main.ml
+++ src/main.ml
@@ -1,21 +1,43 @@
 
 module Web = Opium.Std
 
+module IO = Lwt_io
+open Lwt.Infix
+
 module J = Ezjsonm
 
+type note = {
+  title: string;
+  content: string;
+}
 
-let demo_json =
+let note_to_json { title; content } =
   J.dict [
-    "name", J.string "Bob";
-    "age", J.int 42
+    "title", J.string title;
+    "content", J.string content
   ]
 
-let say_hello =
-  Web.get "/hello" (fun req -> Web.respond' (`Json demo_json))
+let post_note notes =
+  Web.get "/notes/:note" begin fun req ->
+    let note = { title = "Untitled"; content = Web.param req "note" } in
+    notes := note :: !notes;
+    IO.printl ("note = " ^ note.content) >>= fun () ->
+    Lwt.return (Web.respond (`String "OK"))
+end
+
+let get_notes notes =
+  Web.get "/notes" begin fun req ->
+    IO.printlf "requesting %d notes..." (List.length !notes) >>= fun () ->
+    let json_note = J.list note_to_json !notes in
+    Lwt.return (Web.respond (`Json json_note))
+end
+
+let notes: note list ref = ref []
 
 let () =
   Web.App.empty
-  |> say_hello
+  |> get_notes notes
+  |> post_note notes
   |> Web.App.run_command
 
 
