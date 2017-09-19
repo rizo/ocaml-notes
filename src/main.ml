eb3a905047b12d162ff69087357c180c07247545
Rizo Isrof <rizo@odis.io>
2017-09-19 17:06:22 +0100

    Basic web app

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,21 @@
 
-Fmt.pr "hello\n"
+module Web = Opium.Std
+
+module J = Ezjsonm
+
+
+let demo_json =
+  J.dict [
+    "name", J.string "Bob";
+    "age", J.int 42
+  ]
+
+let say_hello =
+  Web.get "/hello" (fun req -> Web.respond' (`Json demo_json))
+
+let () =
+  Web.App.empty
+  |> say_hello
+  |> Web.App.run_command
+
 
